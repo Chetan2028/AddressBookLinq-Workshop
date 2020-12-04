6e42d06d9c0cb9aaa129a023f89f65140baaf77c
Chetan2028 <bmchetan2028@gmail.com>
2020-12-04 23:16:11 +0530

    [Chetan] Refactor : refactored main method

--- AddressBookLinq-Workshop/AddressBookMain.cs
+++ AddressBookLinq-Workshop/AddressBookMain.cs
@@ -12,9 +12,29 @@ namespace AddressBookLinq_Workshop
 
             //create Data table
             DataTable table = new DataTable();
+            addressBookManagement.CreateTable(table);
 
-            addressBookManagement.AddDataToTable(table);
-            addressBookManagement.ViewDataTable(table);
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Press 1 to AddContact \nPress 2 to View Contact \nPress 3 to Exit");
+                int choice = Convert.ToInt32(Console.ReadLine());
+                switch (choice)
+                {
+                    case 1:
+                        addressBookManagement.AddContact(table);
+                        break;
+                    case 2:
+                        addressBookManagement.ViewDataTable(table);
+                        break;
+                    case 3:
+                        flag = false;
+                        break;
+                    default:
+                        Console.WriteLine("Enter valid choice");
+                        break;
+                }
+            }
         }
     }
 }
