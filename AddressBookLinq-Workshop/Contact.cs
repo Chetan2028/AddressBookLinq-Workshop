a2116fcff351d620436bdba9cee6974f7dccb688
Chetan2028 <bmchetan2028@gmail.com>
2020-12-04 22:15:32 +0530

    [Chetan] Add : Added model class for Contact

---
+++ AddressBookLinq-Workshop/Contact.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookLinq_Workshop
+{
+    public class Contact
+    {
+        //Variables are encapsulated
+        private string firstName;
+        private string lastName;
+        private string address;
+        private string city;
+        private string state;
+        private int zip;
+        private string phoneNumber;
+        private string email;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Contact"/> class.
+        /// </summary>
+        /// <param name="firstName">The first name.</param>
+        /// <param name="lastName">The last name.</param>
+        /// <param name="address">The address.</param>
+        /// <param name="city">The city.</param>
+        /// <param name="state">The state.</param>
+        /// <param name="zip">The zip.</param>
+        /// <param name="phoneNumber">The phone number.</param>
+        /// <param name="email">The email.</param>
+        public Contact(string firstName, string lastName, string address, string city, string state, int zip, string phoneNumber, string email)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phoneNumber = phoneNumber;
+            this.email = email;
+        }
+
+
+        /// <summary>
+        /// Gets or sets the first name.
+        /// </summary>
+        /// <value>
+        /// The first name.
+        /// </value>
+        public string FirstName { get => firstName; set => firstName = value; }
+
+        /// <summary>
+        /// Gets or sets the last name.
+        /// </summary>
+        /// <value>
+        /// The last name.
+        /// </value>
+        public string LastName { get => lastName; set => lastName = value; }
+
+        /// <summary>
+        /// Gets or sets the address.
+        /// </summary>
+        /// <value>
+        /// The address.
+        /// </value>
+        public string Address { get => address; set => address = value; }
+
+        /// <summary>
+        /// Gets or sets the city.
+        /// </summary>
+        /// <value>
+        /// The city.
+        /// </value>
+        public string City { get => city; set => city = value; }
+
+        /// <summary>
+        /// Gets or sets the state.
+        /// </summary>
+        /// <value>
+        /// The state.
+        /// </value>
+        public string State { get => state; set => state = value; }
+
+        /// <summary>
+        /// Gets or sets the zip.
+        /// </summary>
+        /// <value>
+        /// The zip.
+        /// </value>
+        public int Zip { get => zip; set => zip = value; }
+
+        /// <summary>
+        /// Gets or sets the phone number.
+        /// </summary>
+        /// <value>
+        /// The phone number.
+        /// </value>
+        public string PhoneNumber { get => phoneNumber; set => phoneNumber = value; }
+
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        /// <value>
+        /// The email.
+        /// </value>
+        public string Email { get => email; set => email = value; }
+    }
+}
+
