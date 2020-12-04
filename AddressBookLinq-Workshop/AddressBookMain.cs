70eeaab9739809067f568dac181ac0130659710c
Chetan2028 <bmchetan2028@gmail.com>
2020-12-04 22:05:12 +0530

    Add project files.

---
+++ AddressBookLinq-Workshop/AddressBookMain.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBookLinq_Workshop
+{
+    public class AddressBookMain
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
